fadf4826e29e24b171679ac377a67d12583eebec
sheiely <sheielynascimento@gmail.com>
2025-07-05 17:11:52 -0300

    ✨ feat(api): adiciona endpoint de lista de mensagens por gestação

--- apps/api/src/modules/mensagens/mensagens.controller.ts
+++ apps/api/src/modules/mensagens/mensagens.controller.ts
@@ -1,8 +1,12 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
-import { CreateMessageDto } from './dto/create-message';
 import { MENSAGENS_SERVICE } from 'src/common/constants';
 import { IMensagensService } from './interface/mensagens.service.interface';
+
+import { Get, Query, Param } from '@nestjs/common';
+import { IsPaginated } from 'src/shared/decorators/Ispaginated';
+import { CreateMessageDto } from './dto/create-message';
 import { activeUserId } from 'src/shared/decorators/activeUserId';
+import { PaginacaoDto } from 'src/common/dto/pagination.dto';
 
 @Controller('mensagens')
 export class MensagensController {
@@ -14,5 +18,21 @@ export class MensagensController {
   @Post()
   async create(@Body() dto: CreateMessageDto, @activeUserId() userId: string) {
     return this.mensagensService.create(dto, userId);
+  }
+
+  @Get(':gestacaoId')
+  @IsPaginated()
+  async findByGestacao(
+    @Param('gestacaoId') gestacaoId: string,
+    @Query() paginacaoDto: PaginacaoDto,
+  ) {
+    const resultado = await this.mensagensService.findByGestacao(
+      gestacaoId,
+      paginacaoDto,
+    );
+
+    console.log('Resultado da busca paginada:', resultado); // ✅ ponto para log
+
+    return resultado;
   }
 }
